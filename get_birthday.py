2f2c2543a31078a5c33b3bdb73aa256089ca0cf4
Kirienko-Alyona <ladybird.miami@gmail.com>
2022-11-03 14:49:53 +0200

    Update get_birthday.py
    code optimization

--- get_birthday.py
+++ get_birthday.py
@@ -11,66 +11,48 @@ users = [{"name": "Raul", "birthday": datetime(year=2017, month=11, day=7)},
     ]
 
 ### Функція get_birthdays_per_week() приймає список словників з іменами людей і днями народження у форматі datetime. Виводить список співробітників у яких день народження цього тижня
-def get_birthdays_per_week(users):  
-    names_users_birthday_Monday = []
-    names_users_birthday_Tuesday = []
-    names_users_birthday_Wednesday = []
-    names_users_birthday_Thursday = []
-    names_users_birthday_Friday = []
-    birthday_on_Monday = "" 
-    birthday_on_Tuesday = "" 
-    birthday_on_Wednesday = ""
-    birthday_on_Thursday = ""
-    birthday_on_Friday = ""
-    current_datetime = datetime.now()
-    current_datetime = current_datetime.timetuple()
-    current_day_year = current_datetime.tm_yday
-    current_year = current_datetime.tm_year
-    birthday_range = current_day_year + 7
+def get_birthdays_per_week(users: list):  
+    user_list = {
+        'Monday': [],
+        'Tuesday': [],
+        'Wednesday': [],
+        'Thursday': [],
+        'Friday': []
+    }
+    
+    current_date = datetime.now()
+   # birthday_range = define_days_interval(current_date)
+    current_day_year = current_date.timetuple().tm_yday
+    current_year = current_date.timetuple().tm_year
+    birthday_range = current_day_year + 7    
     
     for user in users:
-        user_name = user["name"]
-        user_birthday = user["birthday"]
-        user_birthday_tuple = user_birthday.timetuple()
-        user_birthday_day_year = user_birthday_tuple.tm_yday
-        now_year_user_birthday = user_birthday.replace(current_year)
+        user_birthday_day_year = user["birthday"].timetuple().tm_yday
+        now_year_user_birthday = user["birthday"].replace(current_year)
 
-        if current_day_year <= user_birthday_day_year <= birthday_range:
+        if current_day_year < user_birthday_day_year <= birthday_range:
             day_of_week = now_year_user_birthday.strftime('%A')
             
-            if day_of_week == "Sunday" or day_of_week == "Saturday" or day_of_week == "Monday":
-                names_users_birthday_Monday.append(user_name)
-                name_users_Monday = ", ".join(names_users_birthday_Monday)
-                birthday_on_Monday = f"Monday: {name_users_Monday}"
-            elif day_of_week == "Tuesday":
-                names_users_birthday_Tuesday.append(user_name)
-                name_users_Tuesday = ", ".join(names_users_birthday_Tuesday)
-                birthday_on_Tuesday = f"{day_of_week}: {name_users_Tuesday}"    
-            elif day_of_week == "Wednesday":
-                names_users_birthday_Wednesday.append(user_name)
-                name_users_Wednesday = ", ".join(names_users_birthday_Wednesday)
-                birthday_on_Wednesday = f"{day_of_week}: {name_users_Wednesday}"
-            elif day_of_week == "Thursday":
-                names_users_birthday_Thursday.append(user_name)
-                name_users_Thursday = ", ".join(names_users_birthday_Thursday)
-                birthday_on_Thursday = f"{day_of_week}: {name_users_Thursday}"
-            elif day_of_week == "Friday":
-                names_users_birthday_Friday.append(user_name)
-                name_users_Friday = ", ".join(names_users_birthday_Friday)
-                birthday_on_Friday = f"{day_of_week}: {name_users_Friday}"    
+            if day_of_week in ['Sunday', 'Saturday', 'Monday']:
+                user_list.get('Monday').append(user["name"])
+            elif day_of_week == 'Tuesday':
+                user_list.get('Tuesday').append(user["name"])
+            elif day_of_week == 'Wednesday':
+                user_list.get('Wednesday').append(user["name"])
+            elif day_of_week == 'Thursday':
+                user_list.get('Thursday').append(user["name"])
+            elif day_of_week == 'Friday':
+                user_list.get('Friday').append(user["name"])
 
-    if birthday_on_Monday != "":
-        print(birthday_on_Monday)     
-    if birthday_on_Tuesday != "":
-        print(birthday_on_Tuesday)
-    if birthday_on_Wednesday != "":
-        print(birthday_on_Wednesday)     
-    if birthday_on_Thursday != "":
-        print(birthday_on_Thursday)    
-    if birthday_on_Friday != "":
-        print(birthday_on_Friday)  
-           
+    print_user_list(user_list)   
+
     return
 
+def print_user_list(user_list: dict):
+    for key, value in user_list.items():
+        if value:
+            print(f"{key}: {', '.join(value)}")
+
+    return
 
 get_birthdays_per_week(users)
